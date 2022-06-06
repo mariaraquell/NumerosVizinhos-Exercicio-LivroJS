7656613350e3fffd2f9888b40cebb5ed055b4205
Maria Raquel <94325823+mariaraquell@users.noreply.github.com>
2022-06-05 22:59:35 -0300

    Add files via upload

---
+++ ex.LivroJS.js
@@ -0,0 +1,10 @@
+<><meta charset="utf-8>" />
+<script>
+    // dado de entrada
+    var num= Number(prompt("Número: "));
+    // calcula os vizinhos
+    var anterior= num - 1;
+    var posterior= num + 1;
+    //apresentar respostas
+    alert("Vizinhos: " + anterior + " e " + posterior);
+</script>
